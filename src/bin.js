876d601a6671a89a172c3c2426ea4a5a76aa3ae5
Ricky Cook <ricky.cook@cmd.com.au>
2021-10-06 18:17:51 +1100

    Add initial packages

---
+++ src/bin.js
@@ -0,0 +1,100 @@
+const { program, Command } = require('commander');
+
+const {
+  ArgumentError,
+  createAppToken,
+  createInstallationToken,
+  createRepoRunnerRegistrationToken,
+  createOrgRunnerRegistrationToken,
+} = require('./index');
+
+const createAction = fn => async opts => {
+  try {
+    console.log(await fn(opts));
+  } catch(err) {
+    if (opts.debug) {
+      console.error(err);
+    } else {
+      console.error('ERROR:', err.message);
+    }
+    process.exit(1);
+  }
+}
+const appTokenAct = createAction(createAppToken);
+const installationTokenAct = createAction(createInstallationToken);
+const repoRunnerTokenAct = createAction(createRepoRunnerRegistrationToken);
+const orgRunnerTokenAct = createAction(createOrgRunnerRegistrationToken);
+
+const addDebugArgs = cmd => (
+  cmd
+    .option('--debug', 'turn on debug outputs')
+)
+const addAppTokenArgs = cmd => (
+  cmd
+    .option('-a, --appId <id>', 'app ID')
+    .option('-k, --privateKeyPath <path>', 'path to the private key file for the app')
+);
+const addInstallationTokenArgs = cmd => (
+  cmd
+    .option('-i, --installationId <id>', 'installation ID for the app')
+);
+const addOrgNameArgs = cmd => (
+  cmd
+    .option('-o, --orgName <name>', 'organization name to act on')
+);
+const addRepoNameArgs = cmd => (
+  cmd
+    .option('-r, --repoName <name>', 'full name of the repository to act on')
+);
+const addPersonalAccessTokenArgs = cmd => (
+  cmd
+    .option('-p, --personalAccessToken <token>', 'personal access token to use for generation')
+);
+
+const root = program
+  .version(
+    JSON.parse(
+      require('fs').readFileSync(`${__dirname}/../package.json`)
+    ).version,
+  )
+
+const appTokenCmd = program
+  .command('app-token')
+  .description('create an app JWT')
+addDebugArgs(appTokenCmd);
+addAppTokenArgs(appTokenCmd);
+appTokenCmd.action(appTokenAct);
+
+const installationTokenCmd = program
+  .command('installation-token')
+  .description('create an installation token')
+addDebugArgs(installationTokenCmd);
+addAppTokenArgs(installationTokenCmd);
+addInstallationTokenArgs(installationTokenCmd);
+addOrgNameArgs(installationTokenCmd);
+addRepoNameArgs(installationTokenCmd);
+installationTokenCmd.action(installationTokenAct);
+
+const repoRunnerTokenCmd = program
+  .command('repo-runner-token')
+  .description('create a token to manage self-hosted runners on a repo')
+addDebugArgs(repoRunnerTokenCmd);
+addAppTokenArgs(repoRunnerTokenCmd);
+addInstallationTokenArgs(repoRunnerTokenCmd);
+addOrgNameArgs(repoRunnerTokenCmd);
+addRepoNameArgs(repoRunnerTokenCmd);
+addPersonalAccessTokenArgs(repoRunnerTokenCmd);
+repoRunnerTokenCmd.action(repoRunnerTokenAct);
+
+const orgRunnerTokenCmd = program
+  .command('org-runner-token')
+  .description('create a token to manage self-hosted runners on an org')
+addDebugArgs(orgRunnerTokenCmd);
+addAppTokenArgs(orgRunnerTokenCmd);
+addInstallationTokenArgs(orgRunnerTokenCmd);
+addOrgNameArgs(orgRunnerTokenCmd);
+addRepoNameArgs(orgRunnerTokenCmd);
+addPersonalAccessTokenArgs(orgRunnerTokenCmd);
+orgRunnerTokenCmd.action(orgRunnerTokenAct);
+
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,149 @@
+const axios = require('axios');
+const fs = require('fs/promises');
+const jwt = require('jsonwebtoken');
+const uniq = require('lodash/uniq');
+const flatten = require('lodash/flatten');
+
+const rootLog = require('./log');
+
+const defaultApiPrefix = 'https://api.github.com';
+
+const createClient = ({ authorization, apiPrefix = defaultApiPrefix }) => (
+  axios.create({
+    baseURL: apiPrefix,
+    headers: {
+      Authorization: authorization,
+      Accept: 'application/vnd.github.v3+json',
+    },
+  })
+);
+const tryCreateTokens = async (fns, opts) => {
+  const argErrors = [];
+  for (const fn of fns) {
+    try {
+      return await fn(opts);
+    } catch(err) {
+      if (err instanceof ArgumentError) {
+        argErrors.push(err);
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  const allMessages = uniq(argErrors.map(err => err.message));
+  const allFields = uniq(flatten(argErrors.map(err => err.fields)));
+  throw new ArgumentError(
+    allMessages.join(' OR '),
+    allFields,
+  );
+};
+
+class ArgumentError extends Error {
+  constructor(msg, fields) {
+    super(msg);
+    this.fields = fields;
+  }
+}
+exports.ArgumentError = ArgumentError;
+
+exports.createAppToken = async opts => {
+  const { appId, privateKey, privateKeyPath, parentLog = rootLog } = opts;
+  if (!appId)
+    throw new ArgumentError('Must give appId', ['appId']);
+  if (!privateKey && !privateKeyPath)
+    throw new ArgumentError('Must give either privateKey or privateKeyPath', ['privateKey', 'privateKeyPath']);
+
+  const log = parentLog.extend('createAppToken');
+
+  log('Loading PEM');
+  const pem = privateKey
+    ? privateKey
+    : (await fs.readFile(privateKeyPath)).toString();
+
+  log('Creating app token');
+  return jwt.sign({}, pem, {
+    issuer: appId,
+    expiresIn: '10m',
+    algorithm: 'RS256',
+  });
+};
+exports.createAppAuthorization = async opts => (
+  'Bearer ' + await exports.createAppToken(opts)
+);
+
+exports.createInstallationToken = async opts => {
+  if (opts.repoName && !opts.orgName)
+    opts.orgName = opts.repoName.split('/')[0];
+
+  let { installationId } = opts;
+  const { orgName, parentLog = rootLog } = opts;
+  if (!installationId && !orgName)
+    throw new ArgumentError('Must give either installationId, orgName, or repoName', ['installationId', 'orgName', 'repoName']);
+
+  const log = parentLog.extend('createInstallationToken');
+
+  log('Getting installations');
+  const client = createClient({ authorization: await exports.createAppAuthorization(opts) });
+  if (!installationId) {
+    const installations = (await client.get('/app/installations')).data;
+    const installation = installations.find(i => {
+      if (i.account && i.account.type === 'Organization' && i.account.login === orgName) {
+        return true;
+      }
+    });
+    if (!installation) throw new Error('Could not find installation');
+    installationId = installation.id
+  }
+
+  log('Creating installation token');
+  return (await client.post(
+    `/app/installations/${installationId}/access_tokens`
+  )).data.token;
+}
+exports.createInstallationAuthorization = async opts => (
+  'Bearer ' + await exports.createInstallationToken(opts)
+);
+
+exports.getPersonalAccessToken = opts => {
+  if (opts.personalAccessToken) return opts.personalAccessToken;
+  throw new ArgumentError('Must give personlAccessToken', ['personalAccessToken']);
+}
+exports.createPersonalAccessAuthorization = async opts => (
+  'token ' + exports.getPersonalAccessToken(opts)
+);
+
+exports.createOrgRunnerRegistrationToken = async opts => {
+  if (opts.repoName && !opts.orgName)
+    opts.orgName = opts.repoName.split('/')[0];
+
+  const { orgName, parentLog = rootLog } = opts;
+  if (!orgName) // TODO we could get this from installation
+    throw new ArgumentError('Must give orgName or repoName', ['orgName', 'repoName']);
+
+  const log = parentLog.extend('createOrgRunnerRegistrationToken');
+
+  const client = createClient({
+    authorization: await tryCreateTokens([
+      exports.createInstallationAuthorization,
+      exports.createPersonalAccessAuthorization,
+    ], opts),
+  });
+  log('Creating organization runner registration token');
+  return (await client.post(`/orgs/${orgName}/actions/runners/registration-token`)).data.token
+};
+exports.createRepoRunnerRegistrationToken = async opts => {
+  const { repoName, parentLog = rootLog } = opts;
+  if (!repoName) throw new ArgumentError('Must give repoName', ['repoName']);
+
+  const log = parentLog.extend('createRepoRunnerRegistrationToken');
+
+  const client = createClient({
+    authorization: await tryCreateTokens([
+      exports.createInstallationAuthorization,
+      exports.createPersonalAccessAuthorization,
+    ], opts),
+  });
+  log('Creating repo runner registration token');
+  return (await client.post(`/repos/${repoName}/actions/runners/registration-token`)).data.token
+};
